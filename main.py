37a18d7f8a9f3f91a00702f8a4f6484b2785ba77
Hao <hao@haos-mbp-2.home>
2019-11-26 01:29:58 -0500

    11/25 update

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ global exit_flag
 exit_flag=0
 global left_motor
 left_motor={'speed':0,'mode':0}
+global right_motor
+right_motor={'speed':0,'mode':0}
 
 
 def count(num):
@@ -34,9 +36,38 @@ def keyboard_pressed():
         ch=getchar()
         if(ch=='w'):
             print("w is detected")
-            tmp={'speed':1}
-            left_motor.update(tmp)
-            print("now the values are",left_motor.values())
+            left_motor["mode"]=1               #mode 1=forward
+            right_motor["mode"]=1              #mode 1=forward
+            print("now the left values are",left_motor.values())
+            print("now the right values are",right_motor.values())
+        elif(ch=='x'):
+            print("x is detected")
+            left_motor["mode"]=2               #mode 2=backward
+            right_motor['mode']=2              #mode 2=backweard
+            print("now the left values are",left_motor.values())
+            print("now the right values are",right_motor.values())
+        elif(ch=='a'):
+            print("a is detected")
+        elif(ch=='d'):
+            print("d is detected")
+        elif(ch=='i'):
+            print("i is detected")
+            left_tmp=left_motor.get("speed")+4
+            right_tmp=right_motor.get("speed")+4
+            left_motor["speed"]=left_tmp
+            right_motor["speed"]=right_tmp
+        elif(ch=='j'):
+            print("j is detected")
+            left_tmp=left_motor.get("speed")-4
+            right_tmp=right_motor.get("speed")-4
+            left_motor["speed"]=left_tmp
+            right_motor["speed"]=right_tmp
+        elif(ch=='s'):                         #stop mode set to 0
+            print("s is detected")
+            left_motor["speed"]=0
+            left_motor["mode"]=0
+            right_motor["speed"]=0
+            right_motor["mode"]=0
         elif(ch=='c'):
             print("exit the program")
             exit_flag=1
