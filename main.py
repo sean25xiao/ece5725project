7965f2d5b702d84a30ec5bdc1d493d46712599b2
sean25xiao <sean.xyb@icloud.com>
2019-12-02 01:47:45 -0500

    motor rolling

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import time
 from threading import Thread
+import RPi.GPIO as GPIO
+from RpiMotorLib import rpi_dc_lib      #import motor board libarary
 
 #This grtchar code is from github repo, however with a problem that can not
 #catch ctrl+C signal
@@ -23,10 +25,28 @@ left_motor={'speed':0,'mode':0}
 global right_motor
 right_motor={'speed':0,'mode':0}
 
+#define motor GPIO pins and duty cycle
+PWA = 12
+AI1 = 18
+AI2 = 19
+#PWB = 18
+#BI1 = 23
+#BI2 = 24
+Standby = 25
 
-def count(num):
+Freq = 50
+# end of define GPIO pins
+
+def left_motor_thread():
      if (exit_flag==0):
-         num+=1
+           print("START")
+           rpi_dc_lib.TB6612FNGDc.standby(Standby, True)
+           print("motorone tests")
+           #print(" TEST: testing motor 1")
+           MotorOne = rpi_dc_lib.TB6612FNGDc(AI1 ,AI2 ,PWA ,Freq,True, "motor_one")    # Motorssetup
+           print("1. motor forward")
+           MotorOne.forward(100)
+           time.sleep(3)
      else:
           exit()
 
@@ -78,7 +98,7 @@ def keyboard_pressed():
 #key_pressed = Thread(target=keyboard_pressed, args=(i,))
 print("start of the program")
 key_pressed = Thread(target=keyboard_pressed)
-t1=Thread(target=count,args=(1,))
+t1=Thread(target=left_motor_thread)
 print("creat 2 threads")
 key_pressed.start()
 t1.start()
