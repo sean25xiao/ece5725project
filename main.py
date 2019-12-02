8a122c5ad13dc1b7c942e9736f9e14589a262970
Hao <hao@haos-mbp-2.home>
2019-12-02 15:05:26 -0500

    compelete left motor control

--- main.py
+++ main.py
@@ -24,7 +24,10 @@ global left_motor
 left_motor={'speed':0,'mode':0}
 global right_motor
 right_motor={'speed':0,'mode':0}
-
+global sleeptime
+sleeptime=0.05           #50ms
+global turnsleep
+turnsleep=0.5
 #define motor GPIO pins and duty cycle
 PWA = 12
 AI1 = 18
@@ -33,7 +36,6 @@ AI2 = 19
 #BI1 = 23
 #BI2 = 24
 Standby = 25
-
 Freq = 50
 # end of define GPIO pins
 
@@ -42,11 +44,50 @@ def left_motor_thread():
            print("START")
            rpi_dc_lib.TB6612FNGDc.standby(Standby, True)
            print("motorone tests")
-           #print(" TEST: testing motor 1")
            MotorOne = rpi_dc_lib.TB6612FNGDc(AI1 ,AI2 ,PWA ,Freq,True, "motor_one")    # Motorssetup
-           print("1. motor forward")
-           MotorOne.forward(100)
-           time.sleep(3)
+           if (left_motor["mode"]==1):                                                 #forward
+               if (left_motor["speed"]==1):
+                   MotorOne.forward(20)
+                   print("forward duty cycle of 20")
+               elif(left_motor["speed"]==2):
+                   MotorOne.foward(40)
+                   print("forward duty cycle of 40")
+               elif(left_motor["speed"]==3):
+                   MotorOne.foward(60)
+                   print("forward duty cycle of 60")
+               elif(left_motor["speed"]==4):
+                   MotorOne.forward(80)
+                   print("forward duty cycle of 80")
+               elif(left_motor["speed"]==5):
+                    MotorOne.foward(100)
+                    print("forward duty cycle of 100")
+           #time.sleep(sleeptime)
+           elif(left_motor["mode"]==2):                                         #backward
+               if (left_motor["speed"]==1):
+                   MotorOne.backward(20)
+                   print("backward duty cycle of 20")
+               elif(left_motor["speed"]==2):
+                   MotorOne.backward(40)
+                   print("backward duty cycle of 40")
+               elif(left_motor["speed"]==3):
+                   MotorOne.backward(60)
+                   print("backward duty cycle of 60")
+               elif(left_motor["speed"]==4):
+                   MotorOne.backward(80)
+                   print("backward duty cycle of 80")
+               elif(left_motor["speed"]==5):
+                    MotorOne.backward(100)
+                    print("backward duty cycle of 100")
+           elif(left_motor["mode"]==3):
+               MotorOne.stop(0)
+               print("turn left(left)")
+               time.sleep(turnsleep)
+               print("turn finished")
+           elif(left_mode["mode"]==4):
+               print("turn right (left)")
+           elif(left_mode["mode"]==0):
+               MotorOne.stop(0)
+               print("stop(left)")
      else:
           exit()
 
@@ -68,18 +109,22 @@ def keyboard_pressed():
             print("now the right values are",right_motor.values())
         elif(ch=='a'):
             print("a is detected")
+            left_motor["mode"]=3               #mode 3=left turn
+            right_motor["mode"]=3
         elif(ch=='d'):
             print("d is detected")
+            left_motor["mode"]=4               #mode 4=right turn
+            right_motor["mode"]=4
         elif(ch=='i'):
             print("i is detected")
-            left_tmp=left_motor.get("speed")+4
-            right_tmp=right_motor.get("speed")+4
+            left_tmp=left_motor.get("speed")+1
+            right_tmp=right_motor.get("speed")+1
             left_motor["speed"]=left_tmp
             right_motor["speed"]=right_tmp
         elif(ch=='j'):
             print("j is detected")
-            left_tmp=left_motor.get("speed")-4
-            right_tmp=right_motor.get("speed")-4
+            left_tmp=left_motor.get("speed")-1
+            right_tmp=right_motor.get("speed")-1
             left_motor["speed"]=left_tmp
             right_motor["speed"]=right_tmp
         elif(ch=='s'):                         #stop mode set to 0
